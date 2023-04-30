36517a3ede7b5a5b2b01008b66fbe389f973e6cd
Andriikryl <andreyk557@gmail.com>
2023-04-30 16:52:45 +0300

    feat[Board - component]

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-
-import './App.css';
+import "./App.css";
+import Board from "./components/Board/Board";
 
 function App() {
   return (
     <div className="App">
-
+      <Board />
     </div>
   );
 }
---
+++ src/components/Board/Board.js
@@ -0,0 +1,27 @@
+import { getCharacter } from "../../helper";
+
+const Board = () => {
+  const ranks = Array(8)
+    .fill()
+    .map((x, i) => 8 - i);
+  const files = Array(8)
+    .fill()
+    .map((x, i) => getCharacter(i));
+
+  return (
+    <div className="board">
+      <div className="titles">
+        {ranks.map((rank, i) =>
+          files.map((file, j) => (
+            <div>
+              {rank}
+              {file}
+            </div>
+          ))
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Board;
