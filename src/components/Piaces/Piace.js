06e83152e9e8bef54f459e31faee3363a4f97293
Andriikryl <andreyk557@gmail.com>
2023-05-01 22:18:05 +0300

    feat[piece-componetns-drug-and-drop]

--- src/components/Piaces/Piace.js
+++ src/components/Piaces/Piace.js
@@ -1,6 +1,21 @@
 const Piace = ({ rank, file, piece }) => {
+  const onDragStart = (e) => {
+    e.dataTransfer.effectAllowed = "move";
+    e.dataTransfer.setData("text/plain", `${piece}, ${rank},${file}`);
+    setTimeout(() => {
+      e.target.style.display = "none";
+    }, 0);
+  };
+
+  const onDragEnd = (e) => (e.target.style.display = "block");
+
   return (
-    <div className={`piece ${piece} p-${file}${rank}`} draggable={true}></div>
+    <div
+      className={`piece ${piece} p-${file}${rank}`}
+      draggable={true}
+      onDragEnd={onDragEnd}
+      onDragStart={onDragStart}
+    ></div>
   );
 };
 
